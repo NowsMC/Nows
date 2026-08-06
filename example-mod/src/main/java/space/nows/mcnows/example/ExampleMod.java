a0a04334d857301b551354695ec1f8106772ebd8
TamKungZ <kittiwut.pimpromma@gmail.com>
2026-08-07 01:52:56 +0700

    Refactor GEB service retrieval in ExampleMod for improved context handling

--- example-mod/src/main/java/space/nows/mcnows/example/ExampleMod.java
+++ example-mod/src/main/java/space/nows/mcnows/example/ExampleMod.java
@@ -1,5 +1,6 @@
 package space.nows.mcnows.example;
 
+import foo.zaaarf.geb.GEB;
 import net.minecraft.client.Minecraft;
 import space.nows.mcnows.api.ModInitializer;
 import space.nows.mcnows.api.NowsContext;
@@ -11,6 +12,6 @@ public final class ExampleMod implements ModInitializer {
         // Do not force Minecraft's singleton to initialize before Main.main starts.
         System.out.println("[Nows Example] Mojang-named class = " + Minecraft.class.getName());
         System.out.println("[Nows Example] Mods = " + context.mods().size());
-        System.out.println("[Nows Example] GEB = " + space.nows.mcnows.integration.geb.GebIntegration.eventBus(context).getClass().getName());
+        System.out.println("[Nows Example] GEB = " + context.service(GEB.class).getClass().getName());
     }
 }
