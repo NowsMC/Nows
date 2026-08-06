e77d6b3d66b208de6091477354988b8d91957571
TamKungZ <kittiwut.pimpromma@gmail.com>
2026-08-07 01:51:28 +0700

    Add reactor version dependency to mixin integration

--- integrations/mixin/build.gradle.kts
+++ integrations/mixin/build.gradle.kts
@@ -1,9 +1,12 @@
 plugins { `java-library` }
 
 val mixinVersion = providers.gradleProperty("mixin_version")
+val reactorVersion = providers.gradleProperty("reactor_version")
+
 dependencies {
     api(project(":core"))
     implementation(project(":integrations:logging"))
+    compileOnly("io.projectreactor:reactor-core:${reactorVersion.get()}")
     implementation("net.fabricmc:sponge-mixin:${mixinVersion.get()}")
 }
 
