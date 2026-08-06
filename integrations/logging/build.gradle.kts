e5e4f054e21a10593eb0023ac294ef915db655a7
TamKungZ <kittiwut.pimpromma@gmail.com>
2026-08-07 01:56:53 +0700

    Refactor dependencies in build.gradle.kts to use api for reactor-core

--- integrations/logging/build.gradle.kts
+++ integrations/logging/build.gradle.kts
@@ -2,9 +2,10 @@ plugins { `java-library` }
 
 val reactorVersion = providers.gradleProperty("reactor_version")
 val disruptorVersion = providers.gradleProperty("disruptor_version")
+
 dependencies {
     api(project(":core"))
-    implementation("io.projectreactor:reactor-core:${reactorVersion.get()}")
+    api("io.projectreactor:reactor-core:${reactorVersion.get()}")
     implementation("com.lmax:disruptor:${disruptorVersion.get()}")
     compileOnly("org.slf4j:slf4j-api:2.0.17")
 }
