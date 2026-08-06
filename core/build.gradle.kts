32aaa3ec16303cb0a8e152f9faf982ffc4f5f160
TamKungZ <kittiwut.pimpromma@gmail.com>
2026-08-07 01:51:56 +0700

    Add testRuntimeOnly dependency for JUnit Platform Launcher

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -16,4 +16,5 @@ tasks.test { useJUnitPlatform() }
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.12.2"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
