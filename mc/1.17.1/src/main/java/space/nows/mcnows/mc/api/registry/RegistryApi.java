1d46b8df50947fad200e1f2bf70b1b5453b1281d
TamKungZ <kittiwut.pimpromma@gmail.com>
2026-08-22 22:30:03 +0700

    Adapt Minecraft 1.17.1 registry to stable specs

--- mc/1.17.1/src/main/java/space/nows/mcnows/mc/api/registry/RegistryApi.java
+++ mc/1.17.1/src/main/java/space/nows/mcnows/mc/api/registry/RegistryApi.java
@@ -49,6 +49,8 @@ public interface RegistryApi {
 
     Item registerItem(String id);
 
+    Item registerItem(ItemSpec spec);
+
     Item registerItem(String id, ItemLogic logic);
 
     Item registerItem(String id, Function<Item.Properties, Item.Properties> configure);
@@ -82,6 +84,8 @@ public interface RegistryApi {
 
     Block registerBlock(String id);
 
+    Block registerBlock(BlockSpec spec);
+
     Block registerBlock(String id, BlockLogic logic);
 
     Block registerBlock(String id, Function<BlockBehaviour.Properties, BlockBehaviour.Properties> configure);
@@ -95,6 +99,8 @@ public interface RegistryApi {
     BlockItem registerBlockItem(String id, Block block, Function<Item.Properties, Item.Properties> configure);
 
     BlockEntry registerBlockWithItem(String id);
+
+    BlockEntry registerBlockWithItem(BlockSpec spec);
 
     BlockEntry registerBlockWithItem(
             String id,
